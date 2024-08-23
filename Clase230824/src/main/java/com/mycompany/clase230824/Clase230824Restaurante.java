6ad74983a3ef953031c42c1f2ec863268a9bf3d5
Daniel-f56 <dftorradoc@ufpso.edu.co>
2024-08-23 14:36:56 +0200

    Clase del 23/Ago/2024

---
+++ Clase230824/src/main/java/com/mycompany/clase230824/Clase230824Restaurante.java
@@ -0,0 +1,12 @@
+package com.mycompany.clase230824;
+
+public class Clase230824Restaurante {
+
+    public static void main(String[] args) {
+        //El dia de hoy veremos matrices, con un ejemplo de como funciona el restaurante escolar
+        //El proyecto se llama Restaurante
+        Ventana venta = new Ventana();
+        venta.setVisible(true);
+        venta.setLocationRelativeTo(null);
+    }
+}
---
+++ Clase230824/src/main/java/com/mycompany/clase230824/Menu.java
@@ -0,0 +1,41 @@
+package com.mycompany.clase230824;
+
+import javax.swing.JOptionPane;
+
+public class Menu {
+    String platos[];
+    int datos[][];
+    String dias[] = new String[]{"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+    
+    public void crearMenu(int cantidad){
+        
+        platos = new String[cantidad];
+        
+        int i = 0;
+        while(i<cantidad){
+            platos[i] = JOptionPane.showInputDialog("Diga el nombre del plato "+(i+1));
+            i++;
+        }
+        
+    }
+    
+    public void consultarMenu(){
+        
+        for(int i=0; i<platos.length;i++){
+            JOptionPane.showMessageDialog(null, "PLatos: "+platos[i]);
+        }
+        
+    }
+    
+    public void ingresarDatos(){
+        
+        datos = new int[6][platos.length];
+        
+        //for para recorrer la matriz
+        for(int f=0; f<6; f++){
+            for(int c=0;c<platos.length;c++){
+                datos[f][c]=Integer.parseInt(JOptionPane.showInputDialog(null, "Valores para el dia: "+dias[c]+" PLato: "+platos[c]));
+            }
+        }
+    }
+}
